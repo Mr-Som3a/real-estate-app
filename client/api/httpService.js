9c5244a842f4153cab4e717c1ebbe8d9678c51fd
Mr.Som3a <mrsom3a8@gmail.com>
2025-09-05 23:32:22 +0400

    version 2.0

--- client/api/httpService.js
+++ client/api/httpService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // your base API URL
+  baseURL: import.meta.env.VITE_API_URL, // your base API URL
   withCredentials:true
 });
 
